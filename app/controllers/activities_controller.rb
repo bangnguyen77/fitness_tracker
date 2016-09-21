7741ccfabf9c08a8bf80d48251aae53fef514539
Bang Nguyen and Rony Nasr <bang+rony@>
2016-09-21 15:35:35 -0700

    added Activity CRUD capabilities with AJAX

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,10 +1,21 @@
-ActivitiesController < ApplicationController
+class ActivitiesController < ApplicationController
+
   def index
     @activities = Activity.all
   end
 
   def new
     @activity = Activity.new
+  end
+
+  def create
+    @activity = Food.new(activity_params)
+    @activity.user_id = current_user.id
+    @activity.save
+    respond_to do |format|
+      format.html { redirect_to activities_url }
+      format.js
+    end
   end
 
   def show
@@ -15,14 +26,14 @@ ActivitiesController < ApplicationController
     @activity = Activity.find(params[:id])
   end
 
-  def update
-    @activity = Activity.find(params[:id])
-    if @activity.update(activity_params)
-      redirect_to activities_path
-    else
-      render 'edit'
-    end
-  end
+  # def update
+  #   @activity = Activity.find(params[:id])
+  #   if @activity.update(activity_params)
+  #     redirect_to activities_path
+  #   else
+  #     render 'edit'
+  #   end
+  # end
 
   def destroy
     @activity = Activity.destroy(params[:id])
@@ -36,5 +47,4 @@ ActivitiesController < ApplicationController
   def activity_params
     params.require(:activity).permit(:name, :calories)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "page#index"
   devise_for :users
-  resources :foods do
-    resources :activities
-  end
+  resources :foods
+  resources :activities
 end
