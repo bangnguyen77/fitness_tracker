2843b4e7fc2de52a7f9affa6d065ba718242813b
Bang Nguyen and Rony Nasr <bang+rony@>
2016-09-21 11:25:56 -0700

    create CRUD ability

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,44 @@
+class FoodsController < ApplicationController
+  def index
+    @foods = Food.all
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user_id = current_user.id
+    if @food.save
+      redirect_to foods_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @food = Food.find(params[:id])
+    render 'edit'
+  end
+
+  def update
+    @food = Food.find(params[:id])
+    if @food.update(food_params)
+      redirect_to foods_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path
+  end
+
+  private
+  def food_params
+    params.require(:food).permit(:name, :calories)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'page/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "page#index"
+  devise_for :users
+  resources :foods
 end
